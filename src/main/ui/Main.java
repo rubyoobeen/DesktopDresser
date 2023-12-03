723ae1e76a8d9cf1279192566b7e2a7d9e4a8d07
yhong <yhong05@student.cs.ubc.ca>
2023-12-03 05:58:58 -0800

    phase 3 (f2)

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -5,6 +5,6 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) {
         // starts new DesktopDresser application
-        new ClosetUI();
+        new DesktopDresserApp();
     }
 }
