383280e1b8883ab04d4533991f746306501df8eb
yhong <yhong05@student.cs.ubc.ca>
2024-09-17 16:47:25 -0700

    test

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // starts new DesktopDresser application
         new DesktopDresserApp();
+        System.out.println("hello");
     }
 }
